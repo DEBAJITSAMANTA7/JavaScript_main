92a080d05c3410399ed21a7e50fcdfd9fe780918
DEBAJIT <mrshuva888@gmail.com>
2024-10-26 22:14:21 +0530

    Day6-1

--- Part_1/first.js
+++ Part_1/first.js
@@ -1,8 +1,49 @@
-fullname= "Tony Stark";
-console.log(fullname);
-x=null
-console.log(x)
-y=undefined;
-console.log(y)
-$shortname="DEBPAL";
-console.log($shortname);
+// fullname= "Tony Stark";
+// console.log(fullname);
+// x=null
+// console.log(x)
+// y=undefined;
+// console.log(y)
+// $shortname="DEBPAL";
+// console.log($shortname);
+// // -------
+// // before defineing a variable we use let
+// // this we can not do it again redeclearit 
+// // we can update it 
+// let firstName="DEBAJIT SAMANTA MAIN";
+// console.log(firstName);
+// firstName="Cheeti";
+// console.log(firstName);
+// const PI=3.14;
+// console.log(PI);
+// // PI=3.90;
+// // console.log(PI); console.error();
+
+// {
+//      // Block 1
+//      let a=10;
+//      console.log(a);
+// //     let a=100;
+// }
+// {
+//      // Block 2
+//      let a=10;
+//      console.log(a);
+// //     let a=100;
+// }
+// let b=true;
+// let c= 100.1000+12;
+
+// Constant Object
+
+
+
+
+const studentData={
+     fullName: "DEBAJIT SAMANTA",
+     rollNo: 123,
+     phNo: 8388870883,
+};
+studentData.fullName="Pallabi";
+studentData["rollNo"]+=100;
+console.log(studentData);
